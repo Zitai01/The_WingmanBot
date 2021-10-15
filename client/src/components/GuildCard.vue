c0dc5b3d8280bfd8793bb2e46675d29d1bab00f2
Zitai Huang <mywpiedu@gmail.com>
2021-10-15 13:00:49 -0500

    discord guild page

---
+++ client/src/components/GuildCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="guildcard">
+        <h3>{{guild.name}}</h3>
+        <div>Role: Owner</div>
+        <img :src="icon" alt="icon">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'GuildCard',
+    data:()=>({
+        icon:null
+    }),
+    props:[
+        'guild'
+    ],
+    methods:{
+        getIcon(){
+            
+            this.icon =  `https://cdn.discordapp.com/icons/${this.guild.id}/${this.guild.icon}.png`
+        }
+    },
+    mounted(){
+        this.getIcon()
+    }
+}
+</script>
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,27 +1,51 @@
 <template>
 <div>
-   This is home
+   
+    <div v-if="guilds" class="guildlist">
+    <div :key="guild.id" v-for="guild in guilds">
+        <GuildCard :guild="guild" />
+    </div>
+    </div>
+    <div v-else >
+        <h1>Login to see your severs</h1>
+    </div>
+
+
 </div>
     
 </template>
 
 
-
-
 <script>
-
+import axios from 'axios'
+import GuildCard from '../components/GuildCard.vue'
 export default {
     name:'Home',
+    components:{
+        GuildCard
+    },
     data:()=>({
-        authenticated:false,
-        userName:null,
-        avatar:null
+        
+        guilds: null
     }),
     methods:{
-         
+        async getGuilds(){
+            let token = localStorage.token
+            let header = {
+                
+                headers:{
+                    'Authorization':`Bearer ${token}`
+                }
+            } 
+            let result = await axios.get('https://discordapp.com/api/users/@me/guilds',header)
+            let guilds = result.data
+            this.guilds = guilds.filter((guild)=>
+                guild.owner == true
+            )
+        }
     },
     mounted(){
-       
+        this.getGuilds()
     }
 }
 </script>
